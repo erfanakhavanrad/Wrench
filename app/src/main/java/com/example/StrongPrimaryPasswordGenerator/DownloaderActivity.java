3ccbf55d4d398a270a954c9d70f2442a69da0abe
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2020-05-17 00:00:54 +0430

    2.0

--- app/src/main/java/com/example/StrongPrimaryPasswordGenerator/DownloaderActivity.java
+++ app/src/main/java/com/example/StrongPrimaryPasswordGenerator/DownloaderActivity.java
@@ -12,6 +12,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,35 +26,35 @@ public class DownloaderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_downloader);
-
-
-//        @Override
-//        protected void onCreate(Bundle savedInstanceState) {
-//            super.onCreate(savedInstanceState);
-//            setContentView(R.layout.activity_main);
         mUrlEt = findViewById(R.id.urlEt);
         mDownloadUrl = findViewById(R.id.downloadUrl);
+
         mDownloadUrl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mUrlEt.getText() != null && !mUrlEt.getText().toString().equals("")) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                            //permission is denied, request it.
-                            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                            //show popup for run time permission
-                            requestPermissions(permissions, PERMISSION_STORAGE_CODE);
-                        } else {
-                            //permission is granted, start download
-                            startDownloading();
-                        }
-                    } else {
-                        //OS is lower than marshmallow, start download
-                        startDownloading();
-                    }
-                } else{
-                    Toast.makeText(DownloaderActivity.this, "ابتدا لینک را وارد کنید", Toast.LENGTH_SHORT).show();
-                }
+             String url = String.valueOf(mUrlEt.getText());
+           if ( URLUtil.isValidUrl(url)){
+               if (mUrlEt.getText() != null && !mUrlEt.getText().toString().equals("")) {
+                   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                       if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                           //permission is denied, request it.
+                           String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                           //show popup for run time permission
+                           requestPermissions(permissions, PERMISSION_STORAGE_CODE);
+                       } else {
+                           //permission is granted, start download
+                           startDownloading();
+                       }
+                   } else {
+                       //OS is lower than marshmallow, start download
+                       startDownloading();
+                   }
+               } else{
+                   Toast.makeText(DownloaderActivity.this, "ابتدا لینک را وارد کنید", Toast.LENGTH_SHORT).show();
+               }
+           } else{
+               Toast.makeText(DownloaderActivity.this, "Enter a valid link", Toast.LENGTH_SHORT).show();
+           }
             }
         });
     }
